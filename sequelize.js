390d496697f901e747a805ef08814e9b648fa098
pedropestana00 <pedropestana_@hotmail.com>
2026-05-26 18:34:39 +0100

    added PORT to .env, updated database.js and sequelize.js to use new env variable, and updated LoginRegister.tsx to use API_URL from .env

--- sequelize.js
+++ sequelize.js
@@ -14,7 +14,11 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, 
     process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'mysql',
+    dialectOptions: {
+        ssl: {require: true, rejectUnauthorized: false}
+    },
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,  
     pool: {
         max: 10,
         min: 0,
